8d41e229697a9664ee496e6e0977ae738ec6f7fd
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-09-05 14:39:24 +0300

    creating basic migrations users,students,teachers and courses

---
+++ db/migrate/20220905113218_create_teachers.rb
@@ -0,0 +1,4 @@
+class CreateTeachers < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
---
+++ db/migrate/20220905113510_add_student_id_and_teacher_id_to_courses.rb
@@ -0,0 +1,4 @@
+class AddStudentIdAndTeacherIdToCourses < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
