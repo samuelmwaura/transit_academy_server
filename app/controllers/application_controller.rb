8e8ff5b2adfc4b7d3bd0b8b2b6f87c0ab47dcbd4
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-09-08 00:26:46 +0300

    Handling the login in the controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,16 @@ class ApplicationController < Sinatra::Base
     set(:default_content_type,"application/json")
 
     post '/login' do
-       puts params
-       puts User.find_by(username:params[:username],password:params[:password])
+      logging_user = User.find_by(username:params[:username],password:params[:password])
+      if logging_user
+            logged_user = if logging_user.category == "student"
+                Student.find(logging_user.student_id).get_student_and_registrations
+            else
+                Teacher.find(logged_user.teacher_id).get_teacher_and_allocations
+            end
+      else
+            nil
+      end
+    
     end
 end
